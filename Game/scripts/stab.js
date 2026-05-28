b1588b6240148ed227222c8d2a1b011de79a4c03
TheCatSchool <jobaa030@osloskolen.no>
2026-05-28 12:59:41 +0200

    viewing the game as it is now, i feel the need to add stab before eventually finishing other matters, stab is the main function of the game and with it its complete

--- Game/scripts/stab.js
+++ Game/scripts/stab.js
@@ -3,8 +3,9 @@ stabmods = [
         id: 1,
         name: "Revolutionary ideas being spread",
         effect: -2,
-        length: perm
+        length: perm,
         //trigger = 5th turn
+        active: 0
     },
     {
         id: 2,
@@ -12,19 +13,23 @@ stabmods = [
         effect: -5,
         length: 5,
         //trigger = event
+        active: 0,
 
     },
     {
         id: 3,
         name: "strong economy",
         effect: 1,
-        length: con //conditonal
+        length: con, //conditonal
+        active: 0
     },
     {
         id: 4,
         name: "Angry Nobles",
         effect: -1,
-        length: con
+        length: con,
+        active: 0
+
 
 
     },
@@ -33,5 +38,12 @@ stabmods = [
         name: "Bankruptcy",
         effect: -20,
         length: 1,
+        active: 0
     }
 ]
+
+stab = 100;
+
+function stabchange() {
+    
+}
